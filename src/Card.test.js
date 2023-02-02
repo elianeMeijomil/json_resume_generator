4a2b485cd308e8c1e11613733e8aab8ef32d3b7f
Eliane Meijomil <soul@elianemeijomil.com>
2023-02-02 15:25:54 -0300

    removing some files that is not on the task yet

--- src/Card.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Card from './Card';
-
-test('renders jsx to make a resume card', () => {
-  //render(<Card />);
-  const name = screen.getByText(/learn react/i);
-  expect(name).toBeInTheDocument();
-});
